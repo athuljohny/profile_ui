1a0c30f36de2fbb11ea281e4b65fcf8b0d269e13
athul johny <athuljohny123@gmail.com>
2022-11-17 22:56:08 +0530

    yes

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,203 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Profile_ui(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class Profile_ui extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: Colors.white,
+        child: ListView(
+          children: [
+            Container(
+              padding: EdgeInsets.only(right: 20, left: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.arrow_back, color: Colors.black),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.menu),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 15),
+            Center(
+              child: Container(
+                height: 150,
+                width: 150,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            "https://assets.vogue.in/photos/5f3a37acac1b7909f36d6814/2:3/w_2560%2Cc_limit/Mahendra%2520Singh%2520Dhoni%2520fun%2520facts.jpg")),
+                    borderRadius: BorderRadius.circular(100)),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 70, right: 70, top: 30),
+              height: 40,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.network(
+                      "https://1000logos.net/wp-content/uploads/2021/04/Facebook-logo.png",
+                      height: 40,
+                      width: 40),
+                  Image.network(
+                    "https://developers.google.com/static/business-communications/images/logo-guidelines/do-logo-circle.png",
+                    height: 30,
+                    width: 30,
+                  ),
+                  Image.network(
+                    "https://cdn3.iconfinder.com/data/icons/social-media-circle/512/circle-twitter-512.png",
+                    height: 30,
+                    width: 30,
+                  ),
+                  Image.network(
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/1200px-LinkedIn_icon_circle.svg.png",
+                    height: 30,
+                    width: 30,
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 50, right: 50, top: 25),
+              height: 80,
+              child: Column(children: [
+                Text(
+                  "chromicle",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "@hsd",
+                  style: TextStyle(color: Colors.black, fontSize: 15),
+                )
+              ]),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 50, right: 50, top: 5),
+              height: 80,
+              child: Column(
+                children: [
+                  Text(
+                    "Mobile App Developer and open",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  Text(
+                    "Source enthusiastic",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+                margin: const EdgeInsets.only(left: 35, right: 35),
+                height: MediaQuery.of(context).size.height * 0.4,
+                width: MediaQuery.of(context).size.width * 0.2,
+                child: SingleChildScrollView(
+                    child: Column(
+                  children: [
+                    ProfileTile(
+                      txt: 'Privacy',
+                      icon: Icon(
+                        Icons.privacy_tip,
+                        color: Colors.black,
+                      ),
+                    ),
+                    ProfileTile(
+                      txt: 'Purchase history',
+                      icon: Icon(
+                        Icons.history,
+                        color: Colors.black,
+                      ),
+                    ),
+                    ProfileTile(
+                      txt: 'Help and Support',
+                      icon: Icon(
+                        Icons.help,
+                        color: Colors.black,
+                      ),
+                    ),
+                    ProfileTile(
+                      txt: 'Settings',
+                      icon: Icon(
+                        Icons.settings,
+                        color: Colors.black,
+                      ),
+                    ),
+                    ProfileTile(
+                      txt: 'Invite a friend',
+                      icon: Icon(
+                        Icons.person_add,
+                        color: Colors.black,
+                      ),
+                    ),
+                    ProfileTile(
+                      txt: 'Logout',
+                      icon: Icon(
+                        Icons.logout_sharp,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                )))
+          ],
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class ProfileTile extends StatelessWidget {
+  const ProfileTile({
+    Key? key,
+    required this.txt,
+    required this.icon,
+  }) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final String txt;
+  final Icon icon;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      margin: const EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.grey[300],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      child: ListTile(
+        dense: true,
+        title: Text(
+          txt,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+        ),
+        leading: icon,
+        trailing: const Icon(
+          Icons.arrow_forward_ios,
+          color: Colors.black,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
